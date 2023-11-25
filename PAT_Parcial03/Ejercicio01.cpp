68bc438997a1a365107bffd0bc31d0aaf554b5d0
HARRIETT GUZMAN LOPEZ <hguzman@correo.url.edu.gt>
2023-11-25 17:48:45 -0600

    que jode el ejercicio 3

--- PAT_Parcial03/Ejercicio01.cpp
+++ PAT_Parcial03/Ejercicio01.cpp
@@ -1,6 +1,36 @@
 #include "Ejercicio01.h"
+#include <queue>
+
+using namespace std;
 
 Node<int>* Ejercicio01::mergeLists(Node<Node<int>*>* lists)
 {
-    return nullptr;
+    auto aux = [](Node<int>* a, Node<int>* b) { return a->value < b->value; };
+    priority_queue<Node<int>*, vector<Node<int>*>, decltype(aux)> pq(aux);
+
+
+    while (lists != nullptr) {
+        if (lists->value != nullptr) {
+            pq.push(lists->value);
+        }
+        lists = lists->next;
+    }
+
+    Node<int> dummy{ 0 };
+    Node<int>* tail = &dummy;
+
+
+    while (!pq.empty()) {
+        Node<int>* top = pq.top();
+        pq.pop();
+
+        if (top->next != nullptr) {
+            pq.push(top->next);
+        }
+
+        tail->next = top;
+        tail = tail->next;
+    }
+
+    return dummy.next;
 }
--- PAT_Parcial03/Ejercicio02.cpp
+++ PAT_Parcial03/Ejercicio02.cpp
@@ -1,6 +1,34 @@
 #include "Ejercicio02.h"
+#include <unordered_map>
+#include <vector>
+
+using namespace std;
 
 vector<string>* Ejercicio02::findRepeatedDnaSequences(Node<char>* head)
 {
-    return nullptr;
+    vector<string>* resultado = new vector<string>();
+    unordered_map<string, int> mapaAux;
+
+    string curr = "";
+    Node<char>* temp = head;
+
+    while (temp != nullptr) {
+        curr += temp->value;
+
+        if (curr.length() == 10) {
+            if (mapaAux.find(curr) == mapaAux.end()) {
+                mapaAux[curr] = 0;
+            }
+            else if (mapaAux[curr] == 0) {
+                resultado->push_back(curr);
+                mapaAux[curr]++;
+            }
+
+            curr.erase(0, 1);
+        }
+
+        temp = temp->next;
+    }
+
+    return resultado;
 }
